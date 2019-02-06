f2958112550676ea053070bae8c395e56e4cf18e
Rajiv Shah <37875322+yomojo@users.noreply.github.com>
2019-02-06 10:15:18 +0000

    changed name of main.js to start.js

--- _site/js/main.js
+++
@@ -1,9 +0,0 @@
-//call feed script
-$('#divRss').FeedEk({
-    FeedUrl : 'https://rajivshah.co/feed.xml',
-    MaxCount : 3,
-    ShowDesc : true,
-    ShowPubDate: false,
-    DescCharacterLimit:90,
-    TitleLinkTarget:'_self'
-  });
